202688f68648a49c74420e1397d94abadd61715c
andrey_litvishchenko <andi.gwinter@gmail.com>
2025-02-09 21:55:17 +0300

    6 спринт. Поправить чекстайл в классе Node

--- src/manager/Node.java
+++ src/manager/Node.java
@@ -9,7 +9,7 @@ public class Node {
     Node prev;
     Node next;
 
-    public Node (Task task, Node prev, Node next) {
+    public Node(Task task, Node prev, Node next) {
         this.task = task;
         this.prev = prev;
         this.next = next;
